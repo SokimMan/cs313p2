f5689b57e4ce846810b8252b4289f286162f6274
Gage <35212618+SokimMan@users.noreply.github.com>
2020-06-20 15:37:50 -0600

    Add cool face API

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
+    // tell it to use the public directory as one where static files live
+    .use(express.static(path.join(__dirname, '/public')))
+  
+    // views is directory for all template files
+    .set('views', path.join(__dirname, '/views'))
+    .set('view engine', 'ejs')
+
+    .get('/', (req, res) => res.render('pages/index'))
+    //.get('/form.html', (req, res) => res.render('form.html'))
+    .get('/cool', (req, res) => res.send(cool()))
+    //.get('/cool', (req, res) => res.send(cool()))
+
+    // On form submission from form.html call calculateCost function
+    .get('/cost', calculateCost)
+
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+    // Connects with our form submission
+    function calculateCost(request, response) {
+
+    const weight = Number(request.query.weight);
+	const package = request.query.package;
+
+	package = package.toLowerCase();
+
+    // Set up a JSON object of the values we want to pass along to the EJS result page
+	const params = {package: package, weight: weight};
+
+	// Render the response, using the EJS page "result.ejs" in the pages directory
+	response.render('pages/result', params);
+
+    }
